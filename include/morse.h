e8f4a632db7f7f8972b5453db9ae0372623fd189
Oliver Edman Chrantz <o.edman@icloud.com>
2026-05-08 23:59:00 +0200

    Add morse.h header file
    functions:
    morse_init
    morse_start
    morse_stop

---
+++ include/morse.h
@@ -0,0 +1,19 @@
+#ifndef MORSE_H_
+
+#define MORSE_H_
+
+
+
+#include <stdbool.h>
+
+
+
+void morse_init(void);
+
+void morse_start(void); // Börjar spela upp sekvensen
+
+void morse_stop(void); // Stänger av sekvensen
+
+
+
+#endif
