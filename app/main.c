b05360bba80bf75f3a3726a29efc3cadbd4dbd58
Oliver Edman Chrantz <o.edman@icloud.com>
2026-06-20 17:16:45 +0200

    Added morse code into main both puzzle works togehter running at same time

--- app/main.c
+++ app/main.c
@@ -1,8 +1,13 @@
 #define F_CPU 16000000UL
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
+#include <stddef.h>
+
 #include "spi.h"
 #include "timer.h"
+#include "gpio.h"
+#include "morse.h"
 
 const uint8_t SEGMENTS[] = {
     0x3F, // 0
@@ -18,20 +23,18 @@ const uint8_t SEGMENTS[] = {
 };
 
 const uint8_t DIGITS[] = {
-    0xFE,
-    0xFD,
-    0xFB,
-    0xF7
+    0xFE, // Siffra 1
+    0xFD, // Siffra 2
+    0xFB, // Siffra 3
+    0xF7  // Siffra 4
 };
 
 volatile uint8_t display_buffer[4] = {0x3F, 0x3F, 0x3F, 0x3F};
-
 uint16_t counter = 0;
 
 void display_multiplex_callback(void) {
     static uint8_t current_digit = 0;
 
-    
     spi_write(DIGITS[current_digit]);
     spi_write(display_buffer[current_digit]);
     spi_latch();
@@ -54,23 +57,26 @@ void counter_increment_callback(void) {
     display_buffer[3] = SEGMENTS[counter % 10];
 }
 
-
 int main(void) {
-
+    
     spi_init();
     timer_init(); 
+
+    gpio_t* morse_led = gpio_new(6, GPIO_DIRECTION_OUTPUT, NULL);
 
     struct timer *display_timer = timer_new(2, display_multiplex_callback);
     struct timer *counter_timer = timer_new(500, counter_increment_callback);
 
     timer_start(display_timer);
     timer_start(counter_timer);
+    morse_start(morse_led);
+
+    sei(); 
 
     while (1) {
-        
         timer_handler();
-
     }
 
+    gpio_delete(&morse_led);
     return 0;
 }
