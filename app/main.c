9b8fc9db1d06ed6730a35044f043a71b83be53df
Oliver Edman Chrantz <o.edman@icloud.com>
2026-06-20 16:39:31 +0200

    7-segment display working with TIMER and SPI driver

--- app/main.c
+++ app/main.c
@@ -1,20 +1,70 @@
+#define F_CPU 16000000UL
 #include <avr/io.h>
-#include <avr/interrupt.h>
-#include <stddef.h>
-
-#include "gpio.h"
+#include <util/delay.h>
+#include "spi.h"
 #include "timer.h"
-#include "morse.h"
+
+const uint8_t SEGMENTS[] = {
+    0x3F, // 0
+    0x06, // 1
+    0x5B, // 2
+    0x4F, // 3
+    0x66, // 4
+    0x6D, // 5
+    0x7D, // 6
+    0x07, // 7
+    0x7F, // 8
+    0x6F  // 9
+};
+
+const uint8_t DIGITS[] = {
+    0xFE,
+    0xFD,
+    0xFB,
+    0xF7
+};
+
+volatile uint8_t display_buffer[4] = {0x3F, 0x3F, 0x3F, 0x3F};
+
+uint16_t counter = 0;
+
+void display_multiplex_callback(void) {
+    static uint8_t current_digit = 0;
+
+    
+    spi_write(DIGITS[current_digit]);
+    spi_write(display_buffer[current_digit]);
+    spi_latch();
+
+    current_digit++;
+    if (current_digit >= 4) {
+        current_digit = 0;
+    }
+}
+
+void counter_increment_callback(void) {
+    counter++;
+    if (counter > 9999) {
+        counter = 0;
+    }
+
+    display_buffer[0] = SEGMENTS[(counter / 1000) % 10]; 
+    display_buffer[1] = SEGMENTS[(counter / 100) % 10];
+    display_buffer[2] = SEGMENTS[(counter / 10) % 10];
+    display_buffer[3] = SEGMENTS[counter % 10];
+}
+
 
 int main(void) {
-    
-    timer_init();
-    
-    gpio_t* morse_led = gpio_new(6, GPIO_DIRECTION_OUTPUT, NULL);
-        
-    morse_start(morse_led);
 
-    sei(); 
+    spi_init();
+    timer_init(); 
+
+    struct timer *display_timer = timer_new(2, display_multiplex_callback);
+    struct timer *counter_timer = timer_new(500, counter_increment_callback);
+
+    timer_start(display_timer);
+    timer_start(counter_timer);
 
     while (1) {
         
@@ -22,6 +72,5 @@ int main(void) {
 
     }
 
-    gpio_delete(&morse_led);
     return 0;
 }
