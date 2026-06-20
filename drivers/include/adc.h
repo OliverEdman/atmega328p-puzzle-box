c354a728d4d1535340a3357107b44a137ba14a39
Oliver Edman Chrantz <o.edman@icloud.com>
2026-06-20 22:07:01 +0200

    test

--- drivers/include/adc.h
+++ drivers/include/adc.h
@@ -19,6 +19,7 @@ void adc_init(void);
  * @brief Read the raw digital value (0-1023) from a specific ADC channel.
  *
  * @param channel The ADC channel to read from (0-7).
+ *
  * @return 10-bit raw ADC value.
  */
 uint16_t adc_read_raw(uint8_t channel);
@@ -27,6 +28,7 @@ uint16_t adc_read_raw(uint8_t channel);
  * @brief Read the analog voltage (0.0V - 5.0V) from a specific ADC channel.
  *
  * @param channel The ADC channel to read from (0-7).
+ *
  * @return Converted voltage as a float.
  */
 float adc_read_voltage(uint8_t channel);
